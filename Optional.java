448ad5d750a3f6789e423a23673233a28354f4eb
gheraibia <gheraibia@hotmail.com>
2019-04-15 17:25:34 +0200

    Create Optional.java

---
+++ Optional.java
@@ -0,0 +1,47 @@
+//*******************************************************************
+// Welcome to CompileJava!
+// If you experience any issues, please contact us ('More Info')  -->
+// Also, sorry that the "Paste" feature no longer works! GitHub broke
+// this (so we'll switch to a new provider): https://blog.github.com\
+// /2018-02-18-deprecation-notice-removing-anonymous-gist-creation/
+//*******************************************************************
+
+import java.lang.*; // headers MUST be above the first class
+import java.util.*;
+// one class needs to have a main() method
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+   	String result=new Three().getTwo().orElse(new Two()).getOne().get().getName().orElseThrow(()->new NumberFormatException());
+    System.out.println(result);
+  }
+}
+
+class One{
+	String name="kikoo";
+  	public Optional<String>getName(){
+  			return Optional.ofNullable(name);
+    }
+
+
+}
+
+class Two{
+	One one=new One();
+  	public Optional<One>getOne(){
+  			return Optional.of(one);
+    }
+
+
+}
+
+class Three{
+	Two two;
+  	public Optional<Two>getTwo(){
+  			return Optional.ofNullable(two);
+    }
+
+
+}
